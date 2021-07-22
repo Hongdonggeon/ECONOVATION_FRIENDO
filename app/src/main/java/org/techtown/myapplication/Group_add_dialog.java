5a862e4083cf76218f97673717637b5c1bcddb7e
Hongdonggeon <ghd700@naver.com>
2021-07-23 00:03:39 +0900

    feat : 그룹 추가 다이얼로그 창 이메일 입력시 fcm 전송

--- app/src/main/java/org/techtown/myapplication/Group_add_dialog.java
+++ app/src/main/java/org/techtown/myapplication/Group_add_dialog.java
@@ -14,6 +14,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+
 public class Group_add_dialog extends AppCompatActivity {
      Button submitBtn;
      Button cancelBtn;
@@ -21,6 +23,7 @@ public class Group_add_dialog extends AppCompatActivity {
      RecyclerView recyclerView;
      GroupMemberAdapter adapter;
      EditText email_input;
+     private HashMap<String,String> userTokens;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +39,9 @@ public class Group_add_dialog extends AppCompatActivity {
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference();
+
+
+        userTokens = (HashMap<String,String>)intent.getSerializableExtra("userTokens");
 
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,7 +76,39 @@ public class Group_add_dialog extends AppCompatActivity {
             public void onClick(View view) {
                 String member_email = email_input.getText().toString();
                 adapter.addItem(new GroupMember(member_email));
+//                if(!userTokens.isEmpty() && userTokens.containsKey(member_email)) {
+//                    Gson gson = new Gson();
+//                    NotificationModel notificationModel = new NotificationModel();
+//                    notificationModel.to = userTokens.get(member_email);
+//                    notificationModel.notification.title = "FRIENDO";
+//                    notificationModel.notification.text = "초대 수락하시겠습니까";
+//
+//                    RequestBody requestBody = RequestBody.create(gson.toJson(notificationModel), MediaType.parse("application/json; charset=utf8"));
+//                    Request request = new Request.Builder().header("Content-Type", "application/json")
+//                            .addHeader("Authorization", "key=AAAAlzEMvvg:APA91bGHGn5W1uGfO3PKxvn_IGMK41j5b2ArIglH6PG_Py2kRNupE0v0St6YX28St_7ZkOKVs31cjz8psFiHvdMqGgSMnbiUyIvhf0XtbJIhaJ2XsD0X-DHjZAd4LX6BYGjumXUE3Lqh")
+//                            .url("https://gcm-http.googleapis.com/gcm/send")
+//                            .post(requestBody)
+//                            .build();
+//                    OkHttpClient okHttpClient = new OkHttpClient();
+//                    okHttpClient.newCall(request).enqueue(new Callback() {
+//                        @Override
+//                        public void onFailure(@NotNull Call call, @NotNull IOException e) {
+//
+//                        }
+//
+//                        @Override
+//                        public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+//
+//                        }
+//                    });
+//                }
+//                else {
+//                    AlertDialog.Builder dialog = new AlertDialog.Builder(Group_add_dialog.this).setMessage("FRIENDO의 사용자가 아닙니다.");
+//                    dialog.create().show();
+//                }
+
                 email_input.setText(null);
+
             }
         });
         recyclerView.setAdapter(adapter);
